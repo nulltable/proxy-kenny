e0939a3dc8a3e52898aa93e2f568c727b5a25ac4
Kenny Szeto <kenny.szeto1@gmail.com>
2019-06-15 16:14:39 -0700

    set up proxy, connected routes to service, added stress test

--- server/index.js
+++ server/index.js
@@ -1,10 +1,14 @@
+require('newrelic');
 const express = require('express');
 const path = require('path');
+const proxy = require('http-proxy-middleware');
 const app = express();
 
 app.use(express.static(path.resolve(__dirname, '../public')));
 
-app.get('/:id', (req, res) => {
+app.use('/search', proxy('http://localhost:3030/search'));
+
+app.get('/', (req, res) => {
   if (!req.params.id) {
     res.status(400);
     res.end();
